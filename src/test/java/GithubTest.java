6ed9e08bab2c55a376817f4a85f9f36019dda50d
Evgeniia Kharitonova <kuznetsovaevgeni@gmail.com>
2025-06-18 00:39:31 +0300

    added answer to 1 question

--- src/test/java/GithubTest.java
+++ src/test/java/GithubTest.java
@@ -22,6 +22,7 @@ public class GithubTest {
         open("/selenide/selenide");
         // клик по wiki
         $("#wiki-tab").click();
+
         // фильтрация по страницам справа
         $("#wiki-pages-filter").setValue("soft");
         //проверка, что SoftAssertions в списке страниц и видима, т.е. отфильтровалась
@@ -30,6 +31,5 @@ public class GithubTest {
         $("[data-filterable-for=wiki-pages-filter]").$(byText("SoftAssertions")).click();
         // проверка, что есть заголовок с примером кода на JUnite5
         $("#user-content-3-using-junit5-extend-test-class").preceding(0).shouldHave(text("3. Using JUnit5 extend test class:"));
-
     }
 }
